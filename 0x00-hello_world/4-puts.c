a42747bade070f82980415cebb285f1e017f0394
Amani-taye <edomaman@gmail.com>
2023-06-16 13:13:49 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main entry point A program that prints a line using puts function
+ * Return : 0 (Success)
+ */
+int main(void) /*main function starts here*/
+{
+/*
+ * main function starts here
+ */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+/*
+ * main functions ends here
+ */
