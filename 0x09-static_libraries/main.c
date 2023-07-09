410072506bcf5484bb3da7503e8db0fd3db23d57
Amani-taye <edomaman@gmail.com>
2023-07-09 12:36:32 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints a moto
+ *
+ * Return: 0
+ * AT
+ */
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
